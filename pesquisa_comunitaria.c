e3ae3b4746b1a3c8716334c65ec9b8b7412d1a8e
Esther Araújo <esthersss09@outlook.com>
2022-05-19 22:37:05 -0300

    Rename Pesquisa Comunitária de Saúde .c to pesquisa_comunitaria.c

--- pesquisa_comunitaria.c
+++ pesquisa_comunitaria.c
@@ -5,11 +5,11 @@
 Autora: ESTHER DE SANTANA ARAUJO
 Componente Curricular: Algoritmos I
 Concluido em: 11/10/2017
-Declaro que este cÛdigo foi elaborado por mim de forma individual e n„o contÈm nenhum
-trecho de cÛdigo de outro colega ou de outro autor, tais como provindos de livros e
-apostilas, e p·ginas ou documentos eletrÙnicos da Internet. Qualquer trecho de cÛdigo
-de outra autoria que n„o a minha est· destacado com uma citaÁ„o para o autor e a fonte
-do cÛdigo, e estou ciente que estes trechos n„o ser„o considerados para fins de avaliaÁ„o.
+Declaro que este c√≥digo foi elaborado por mim de forma individual e n√£o cont√©m nenhum
+trecho de c√≥digo de outro colega ou de outro autor, tais como provindos de livros e
+apostilas, e p√°ginas ou documentos eletr√¥nicos da Internet. Qualquer trecho de c√≥digo
+de outra autoria que n√£o a minha est√° destacado com uma cita√ß√£o para o autor e a fonte
+do c√≥digo, e estou ciente que estes trechos n√£o ser√£o considerados para fins de avalia√ß√£o.
 ******************************/
 
 #include <stdio.h>
